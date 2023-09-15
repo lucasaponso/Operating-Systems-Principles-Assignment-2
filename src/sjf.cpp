5ce63e5d491932f3736602673013991d1ede937d
Lucas Aponso <lucasaponso@outlook.com>
2023-09-15 11:06:13 +1000

    Added calculation of burst time, response time, turnaround time and waiting time for each process. Also have calculated the average turnaround time and the average waiting time.

--- src/sjf.cpp
+++ src/sjf.cpp
@@ -1,5 +1,9 @@
 #include "../includes/pcb.h"
 #include "../includes/sjf.h"
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
 
 bool compareByBurstTime(const pcb& a, const pcb& b) 
 {
@@ -9,20 +13,38 @@ bool compareByBurstTime(const pcb& a, const pcb& b)
 void run(std::queue<pcb>& processes) 
 {
     std::vector<pcb> processVector;
+    int currentTime = 0;
+    int totalTurnaroundTime = 0;
+    int totalWaitingTime = 0;
+
     while (!processes.empty()) 
     {
         processVector.push_back(processes.front());
         processes.pop();
     }
+
     std::sort(processVector.begin(), processVector.end(), compareByBurstTime);
+
+    std::cout << "Contents of the queue after SJF scheduling:" << std::endl;
+    
     for (const auto& p : processVector) 
     {
-        processes.push(p);
+        int responseTime = currentTime;
+        int turnaroundTime = currentTime + p.burst_time;
+        int waitingTime = currentTime;
+
+        totalTurnaroundTime += turnaroundTime;
+        totalWaitingTime += waitingTime;
+
+        std::cout << "ID: " << p.id << ", Burst Time: " << p.burst_time;
+        std::cout << " Response Time: " << responseTime << " Turnaround Time: " << turnaroundTime << " Waiting Time: " << waitingTime << std::endl;
+
+        currentTime += p.burst_time;
     }
-    std::cout << "Contents of the queue after SJF scheduling:" << std::endl;
-    while (!processes.empty()) 
-    {
-        std::cout << "ID: " << processes.front().id << ", Burst Time: " << processes.front().burst_time << std::endl;
-        processes.pop();
-    }
+
+    double avgTurnaroundTime = static_cast<double>(totalTurnaroundTime) / processVector.size();
+    double avgWaitingTime = static_cast<double>(totalWaitingTime) / processVector.size();
+
+    std::cout << "Average Turnaround Time: " << avgTurnaroundTime << std::endl;
+    std::cout << "Average Waiting Time: " << avgWaitingTime << std::endl;
 }
